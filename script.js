2061a509030cd95778d1403ef21769596a5342a3
Patrick Eduardo de Souza Da Silva <109807011+Patrick-souza-silva@users.noreply.github.com>
2024-05-06 00:09:14 -0300

    Finalização para lançar na branch master

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// pegando classe button e atribuindo a uma variável  BUTTON
+var button = document.getElementsByClassName("btn")
+//Fazendo uma condição para percorrer todos os BUTTON
+for (var i = 0; i < button.length; i++){
+    //Função para pegar o botão selcionado e adicionar um evento de click
+    button[i].addEventListener("click", function(){
+        //Evento de click vai direcionar para uma aba fora da página
+        var url = "https://wa.me/5546988090298?text=Ol%C3%A1%20equipe%20Ca-F%C3%89!%20%E2%98%95"
+        window.open(url, "_blank")
+    })
+}
+
